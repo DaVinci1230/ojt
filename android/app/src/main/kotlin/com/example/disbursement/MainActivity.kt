a4160a74e4694fecca76f1bbba47729cc950dcab
DaVinci1230 <angelo.burgos.habaradas@gmail.com>
2024-07-10 10:53:57 +0800

    changes

--- android/app/src/main/kotlin/com/example/disbursement/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.disbursement
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/ojt/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ojt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
